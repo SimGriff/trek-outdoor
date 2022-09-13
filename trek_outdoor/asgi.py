cad87f0519beda3b5633e6f972e0943adfaf2dc8
SimGriff <simgrif@gmail.com>
2022-09-13 10:11:22 +0000

    Installed django

---
+++ trek_outdoor/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for trek_outdoor project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trek_outdoor.settings')
+
+application = get_asgi_application()
