0118c6948a3159bfaba6b6bf534ef69fe4ba1582
SimGriff <simgrif@gmail.com>
2022-09-29 14:46:51 +0000

    Added admin.py to reviews app

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+
+    model = Review
+    list_display = (
+        'product',
+        'user',
+        'rating',
+        'review_text',
+        'date',
+    )
+
+    ordering = ('-rating', '-date')
+    list_filter = ('rating')
+
+
+admin.site.register(Review, ReviewAdmin)
