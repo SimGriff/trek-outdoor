39b0bf7c1ba032b36cce820df294103b8a1998bc
SimGriff <simgrif@gmail.com>
2022-10-02 14:16:44 +0000

    Set debug

--- trek_outdoor/settings.py
+++ trek_outdoor/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['simgriff-trek-outdoor.herokuapp.com', 'localhost']
 
